fa25ea65eadca9002c7b130a04b634b194fb0029
serkankayaa <srknnkaya@gmail.com>
2019-07-23 22:27:31 +0300

    edited ui

--- src/assets/js/tang.js
+++ src/assets/js/tang.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    
     //class a click edildiğinde şekil hareket ettirilir.
     $('.tang-drag').draggable({
         containment:'window',
@@ -9,18 +8,15 @@ $(document).ready(function() {
         snapTolerance: 10,
     });
 
-    //sağ click ile şeklin rotasyonu değişir.
+    //çift click ile şeklin rotasyonu 90 derecelik açılarla değişir.
     var pain = 90;
-
     $( ".tang-drag" ).dblclick(function() {
-
         $(this).css ({
             '-webkit-transform': 'rotate(' + pain + 'deg)',
                '-moz-transform': 'rotate(' + pain + 'deg)',
                  '-o-transform': 'rotate(' + pain + 'deg)',
                 '-ms-transform': 'rotate(' + pain + 'deg)'
         });
-
         pain += 90;    
     });
 });
