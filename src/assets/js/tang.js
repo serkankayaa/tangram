3452b38faf9b9a83a0ed58f6df3a2584273cd457
serkankayaa <srknnkaya@gmail.com>
2019-07-24 14:06:39 +0300

    edited rotation

--- src/assets/js/tang.js
+++ src/assets/js/tang.js
@@ -1,23 +1,46 @@
 $(document).ready(function() {
-    
+
+    $('.shp-rotate').hide();
+    $('.tang-drag').hover(function(){
+        $(this).children('.shp-rotate').show();
+    }, function(){
+        $(this).children('.shp-rotate').hide();
+    });
+
     //class a click edildiğinde şekil hareket ettirilir.
     $('.tang-drag').draggable({
         containment:'window',
         stack: '.tang-drag',
         snap: '.target-shape, .tang-drag', // hem şekiller arası hem de hedef puzzle arasında otomatik hizalama yapar.
         snapMode: 'inner',
-        snapTolerance: 10,
+        snapTolerance: 20,
     });
 
-    //çift click ile şeklin rotasyonu 90 derecelik açılarla değişir.
-    var pain = 90;
-    $( ".tang-drag" ).dblclick(function() {
-        $(this).css ({
-            '-webkit-transform': 'rotate(' + pain + 'deg)',
-               '-moz-transform': 'rotate(' + pain + 'deg)',
-                 '-o-transform': 'rotate(' + pain + 'deg)',
-                '-ms-transform': 'rotate(' + pain + 'deg)'
-        });
-        pain += 90;    
+    //çift click ile şeklin rotasyonu kare ise 45 değilse 90 derecelik açılarla değişir.
+    var nonSquarePain = 90;
+    var squarePain = 45;
+    $('.shp-rotate').click(function() {
+        var squareId = $(this).parent().attr('id');
+        var checkSquare = squareId.includes("square");
+
+        if(checkSquare) {
+            $(this).parent().css ({
+                '-webkit-transform': 'rotate(' + squarePain + 'deg)',
+                   '-moz-transform': 'rotate(' + squarePain + 'deg)',
+                     '-o-transform': 'rotate(' + squarePain + 'deg)',
+                    '-ms-transform': 'rotate(' + squarePain + 'deg)'
+            });
+        }
+        else {
+            $(this).parent().css ({
+                '-webkit-transform': 'rotate(' + nonSquarePain + 'deg)',
+                   '-moz-transform': 'rotate(' + nonSquarePain + 'deg)',
+                     '-o-transform': 'rotate(' + nonSquarePain + 'deg)',
+                    '-ms-transform': 'rotate(' + nonSquarePain + 'deg)'
+            });
+        }
+
+        squarePain += 45;
+        nonSquarePain += 90;    
     });
 });
